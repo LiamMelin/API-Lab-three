514513602b73f940aea96e8446d2bac8fd9d49aa
Priscilla Silva <60390444+priscilla-silva@users.noreply.github.com>
2020-02-17 23:45:09 +0100

    Add files via upload
    I created a blank template containing the html document and the js document that has worked for me so far. I haven't done anything fancy, just basic shapes and spinning animations, but since some of you mentioned you haven't been able to link the library to the DOM, here's what I did.

---
+++ Two JS Blank Template/main.js
@@ -0,0 +1,10 @@
+var elem = document.getElementById(' '); //Add the id of your div inside the quotes//
+var params = { fullscreen: true }; //To set whole browser window as the canvas; otherwise, specify dimensions//
+var two = new Two(params).appendTo(elem); //An instance of Two, which is necessary to append the TwoJS to the DOM//
+
+//Begin your TwoJS example here...//
+
+
+
+
+
